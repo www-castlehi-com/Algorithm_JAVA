538fca18343f52fd93a4d68152ac0b6af02e2613
moong2 <www.castlehi@gmail.com>
2022-03-22 18:20:07 +0900

    220322

---
+++ src/P_11727.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class P_11727 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] rectangle = new int[1001];
+
+        rectangle[1] = 1;
+        rectangle[2] = 3;
+        for (int i = 3; i <= n; i++) rectangle[i] = (rectangle[i - 1] + rectangle[i - 2] * 2) % 10007;
+
+        bw.write(Integer.toString(rectangle[n]));
+        bw.flush();
+    }
+}
