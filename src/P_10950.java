3d51235294d3d6ae9b66228da78fd474cfc8e542
moong2 <www.castlehi@gmail.com>
2022-05-19 14:45:11 +0900

    220519

---
+++ src/P_10950.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class P_10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            bw.write((arr[0] + arr[1]) + "\n");
+        }
+        bw.flush();
+    }
+}
