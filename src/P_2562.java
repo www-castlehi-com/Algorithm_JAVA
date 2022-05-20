a486611a53bde1d33b6af22d89b72ee4d7764592
moong2 <www.castlehi@gmail.com>
2022-05-20 15:01:19 +0900

    220520

---
+++ src/P_2562.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class P_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = Integer.MIN_VALUE, idx = 0;
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > max) {
+                max = num;
+                idx = i;
+            }
+        }
+
+        bw.write(max + "\n" + idx);
+        bw.flush();
+    }
+}
---
+++ src/P_2675.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class P_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String[] s = br.readLine().split(" ");
+
+            int r = Integer.parseInt(s[0]);
+            String repeat = "";
+            for (int j = 0; j < s[1].length(); j++) {
+                for (int k = 0; k < r; k++) repeat += s[1].charAt(j);
+            }
+            bw.write(repeat + "\n");
+        }
+        bw.flush();
+    }
+}
