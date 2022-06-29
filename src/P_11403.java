70fb071331ae65444e647f4472a27efd182a136d
moong2 <www.castlehi@gmail.com>
2022-06-29 20:03:51 +0900

    220629

---
+++ src/P_11403.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class P_11403 {
+
+    static int[][] matrix;
+    static boolean[][] visited;
+    static ArrayList<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        matrix = new int[n][n];
+        visited = new boolean[n][n];
+        graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+            matrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) graph[i].add(j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) bfs(i);
+
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                bw.write(anInt + " ");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+    private static void bfs(int idx) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(idx);
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+
+            for (Integer integer : graph[poll]) {
+                if (!visited[idx][integer]) {
+                    visited[idx][integer] = true;
+                    matrix[idx][integer] = 1;
+                    queue.add(integer);
+                }
+            }
+        }
+    }
+}
---
+++ src/P_5430.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class P_5430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String command = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String xs = br.readLine();
+
+            ArrayDeque<Integer> numbers = new ArrayDeque<>();
+            int num = 0;
+            for (int i = 0; i < xs.length(); i++) {
+                if (xs.charAt(i) >= '0' && xs.charAt(i) <= '9') num = num * 10 + xs.charAt(i) - '0';
+                else if (xs.charAt(i) == ',' || xs.charAt(i) == ']') {
+                    if (num != 0) {
+                        numbers.add(num);
+                        num = 0;
+                    }
+                }
+            }
+
+            boolean error = false;
+            boolean reverse = false;
+            for (int i = 0; i < command.length(); i++) {
+                if (command.charAt(i) == 'R') reverse = !reverse;
+                else {
+                    if (numbers.isEmpty()) {
+                        error = true;
+                        bw.write("error\n");
+                        break;
+                    }
+                    else {
+                        if (reverse) numbers.pollLast();
+                        else numbers.pollFirst();
+                    }
+                }
+            }
+
+            if (!error) {
+                bw.write("[");
+                if (!numbers.isEmpty()) {
+                    if (reverse) {
+                        bw.write(Integer.toString(numbers.pollLast()));
+                        while (!numbers.isEmpty()) {
+                            bw.write("," + numbers.pollLast());
+                        }
+                    } else {
+                        bw.write(Integer.toString(numbers.pollFirst()));
+                        while (!numbers.isEmpty()) {
+                            bw.write("," + numbers.pollFirst());
+                        }
+                    }
+                }
+                bw.write("]\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
