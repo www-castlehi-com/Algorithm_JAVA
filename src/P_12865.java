0bda956e347bccf0a71cd6d025a47d711c292841
moong2 <www.castlehi@gmail.com>
2022-07-10 20:24:48 +0900

    220710

---
+++ src/P_12865.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class P_12865 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] s = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = s[0], k = s[1];
+        int[][] dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            s = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int w = s[0], v = s[1];
+
+            for (int j = 1; j <= k; j++) {
+                if (j - w >= 0) dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        bw.write(Integer.toString(dp[n][k]));
+        bw.flush();
+    }
+}
--- src/P_SDX_Summer_Prev_No2_User.java
+++ src/P_SDX_Summer_Prev_No2_User.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 class P_SDX_Summer_Prev_No2_User {
 
-    //treeset"?
-    
-    static PriorityQueue<User>[][] pqMax = new PriorityQueue[2][4];
-    static PriorityQueue<User>[][] pqMin = new PriorityQueue[2][4];
-    static HashMap<Integer, User> map;
+    static TreeSet<User>[][] usersSet;
+    static HashMap<Integer, User> usersMap;
 
     static class User {
         int id;
@@ -23,20 +20,18 @@ class P_SDX_Summer_Prev_No2_User {
     }
 
     public void init() {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-                pqMax[i][j] = new PriorityQueue<>((o1, o2) -> {
-                    if (o1.score == o2.score) return o2.id - o1.id;
-                    return o2.score - o1.score;
-                });
+        usersSet = new TreeSet[2][4];
 
-                pqMin[i][j] = new PriorityQueue<>((o1, o2) -> {
-                        if (o1.score == o2.score) return o1.id - o2.id;
-                        return o1.score - o2.score;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0 ; j< 4; j++) {
+                usersSet[i][j] = new TreeSet<>((o1, o2) -> {
+                    if (o1.score == o2.score) return o2.id - o1.id;
+                    else return o2.score - o1.score;
                 });
             }
         }
-        map = new HashMap<>();
+
+        usersMap = new HashMap<>();
     }
 
     public int add(int mId, int mGrade, char mGender[], int mScore) {
@@ -44,39 +39,42 @@ class P_SDX_Summer_Prev_No2_User {
         if (mGender[0] == 'f') sex = 1;
 
         User user = new User(mId, mGrade, sex, mScore);
-        map.put(mId, user);
-        pqMax[sex][mGrade].add(user);
-        pqMin[sex][mGrade].add(user);
+        usersSet[sex][mGrade].add(user);
+        usersMap.put(mId, user);
 
-        return pqMax[sex][mGrade].peek().id;
+        return usersSet[sex][mGrade].first().id;
     }
 
     public int remove(int mId) {
-        if (!map.containsKey(mId)) return 0;
+        if (!usersMap.containsKey(mId)) return 0;
         else {
-            User remove = map.remove(mId);
+            User remove = usersMap.remove(mId);
 
-            pqMax[remove.gender][remove.grade].remove(remove);
-            pqMin[remove.gender][remove.grade].remove(remove);
-
-            if (pqMin[remove.gender][remove.grade].size() == 0) return 0;
-            else return pqMin[remove.gender][remove.grade].peek().id;
+            usersSet[remove.gender][remove.grade].remove(remove);
+            if (usersSet[remove.gender][remove.grade].isEmpty()) return 0;
+            else return usersSet[remove.gender][remove.grade].last().id;
         }
     }
 
     public int query(int mGradeCnt, int mGrade[], int mGenderCnt, char mGender[][], int mScore) {
-        int minScore = Integer.MAX_VALUE;
-        int minId = Integer.MAX_VALUE;
+        TreeSet<User> querySet = new TreeSet<>((o1, o2) -> {
+            if (o1.score == o2.score) return o1.id - o2.id;
+            else return o1.score - o2.score;
+        });
+
         for (int i = 0; i < mGenderCnt; i++) {
             int sex = 0;
             if (mGender[i][0] == 'f') sex = 1;
+
             for (int j = 0; j < mGradeCnt; j++) {
-                if (pqMin[sex][mGrade[j]].size() != 0) {
-                }
+                User target = new User(0, mGrade[j], sex, mScore);
+                User floor = usersSet[sex][mGrade[j]].floor(target);
+
+                if (floor != null) querySet.add(floor);
             }
         }
 
-        if (minId == Integer.MAX_VALUE) return 0;
-        else return minId;
+        if (querySet.isEmpty()) return 0;
+        else return querySet.first().id;
     }
 }
