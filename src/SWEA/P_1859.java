4fbfa776eaa02b13d5ed261245f493a360284498
박성하 <67732143+moong2@users.noreply.github.com>
2023-11-17 22:03:03 +0900

    231117

---
+++ src/SWEA/P_1859.java
@@ -0,0 +1,39 @@
+package SWEA;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		int idx = 1;
+		while (idx <= t) {
+			int n = Integer.parseInt(br.readLine());
+			int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			
+			long res = 0;
+			int i = n - 1;
+			while (i > 0) {
+				long max = line[i];
+				int j = i - 1;
+				
+				long buy = 0;
+				long cnt = 0;
+				while (j >= 0 && line[j] < max) {
+					buy += line[j];
+					j--;
+					cnt++;
+				}
+				res += (cnt * max - buy);
+				
+				i = j;
+			}
+			
+			bw.write("#" + idx++ + " " + res + "\n");
+		}
+		bw.flush();
+	}
+}
