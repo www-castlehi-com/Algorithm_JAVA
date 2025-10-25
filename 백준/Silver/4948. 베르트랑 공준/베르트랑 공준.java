13dce917acad082be52244b6026a6693bcde8c49
박성하 <67732143+www-castlehi-com@users.noreply.github.com>
2025-10-26 00:25:30 +0900

    [Silver II] Title: 베르트랑 공준, Time: 192 ms, Memory: 16348 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,41 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     
-    private static int max = 123_456;
-    private static boolean[] isComposite = new boolean[2 * max + 1];
+    private static int MAX_VALUE = 123_456 * 2;
+    
+    private static boolean[] isPrime = new boolean[MAX_VALUE + 1];
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
-        calculatePrime();
+        initializePrime();
         
         while (true) {
-            int testCase = Integer.parseInt(br.readLine());
-            if (testCase == 0) {
-                break;
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                break ;
             }
             
-            int cnt = 0;
-            for (int i = testCase + 1; i <= 2 * testCase; i++) {
-                if (isComposite[i] == false) {
-                    cnt++;
+            int count = 0;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (isPrime[i]) {
+                    count++;
                 }
             }
-            bw.write(cnt + "\n");
+            
+            sb.append(count + "\n");
         }
         
-        bw.flush();
+        System.out.print(sb);
     }
     
-    private static void calculatePrime() {
-        isComposite[1] = true;
-        for (int i = 2; i * i <= 2 * max; i++) {
-            if (!isComposite[i]) {
-                for (int j = 2 * i; j <= 2 * max; j += i) {
-                    isComposite[j] = true;
+    private static void initializePrime() {
+        Arrays.fill(isPrime, true);
+        isPrime[1] = false;
+        
+        for (int i = 2; i * i <= MAX_VALUE; i++) {
+            if (isPrime[i]) {
+                for (int j = i + i; j <= MAX_VALUE; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
