205e7a1ccf99c4d65f3d7674233db277fbaa7ecb
박성하 <67732143+www-castlehi-com@users.noreply.github.com>
2026-02-03 23:16:33 +0900

    [Gold III] Title: IQ Test, Time: 104 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/1111. IQ Test/IQ Test.java
+++ 백준/Gold/1111. IQ Test/IQ Test.java
@@ -2,74 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    private static int n;
-    private static int[] numbers;
-    private static List<int[]> rules = new LinkedList<>();
-    
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		
-		n = Integer.parseInt(br.readLine());
-		numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		
+		int n = Integer.parseInt(br.readLine());
+		int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		if (n == 1) {
 		    System.out.print("A");
-		} else if (n >= 2) {
-		    initializeRules();
-		    System.out.print(getNextNumber());
-		}
-	}
-	
-	private static void initializeRules() {
-	    for (int i = -200; i <= 200; i++) {
-	        for (int j = -20100; j <= 20100; j++) {
-	            if (numbers[0] * i + j == numbers[1]) {
-	                rules.add(new int[]{i, j});
-	            }
-	        }
-	    }
-	}
-	
-	private static String getNextNumber() {
-	    for (int i = 0; i < n - 1; i++) {
-	        int curNumber = numbers[i];
-	        int nextNumber = numbers[i + 1];
-	        rules.removeIf(rule -> curNumber * rule[0] + rule[1] != nextNumber);
-	    }
-	    
-	    return calculateNextNumber();
-	}
-	
-	private static String calculateNextNumber() {
-	    String nextNumber;
-	    if (rules.isEmpty()) {
-	        nextNumber = "B";
-	    } else if (isAllResultSame()) {
-	        int[] rule = rules.get(0);
-	        nextNumber = String.valueOf(numbers[n - 1] * rule[0] + rule[1]);
-	    } else {
-	        nextNumber = "A";
-	    }
-	    
-	    return nextNumber;
-	}
-	
-	private static boolean isAllResultSame() {
-	    boolean isSame = true;
-	    int nextNumber = 0;
-	    for (int[] rule : rules) {
-	        if (nextNumber == 0) {
-	            nextNumber = numbers[n - 1] * rule[0] + rule[1];
-	        } else {
-	            if (numbers[n - 1] * rule[0] + rule[1] != nextNumber) {
-	                isSame = false;
+		} else if (n == 2) {
+		    if (numbers[0] == numbers[1]) {
+		        System.out.print(numbers[0]);
+		    } else {
+		        System.out.print("A");
+		    }
+		} else {
+		    int a = 0;
+		    if (numbers[0] - numbers[1] != 0) {
+		        a = (numbers[1] - numbers[2]) / (numbers[0] - numbers[1]);
+		    }
+		    int b = numbers[1] - (numbers[0] * a);
+		        
+	        boolean isSolved = true;
+	        for (int i = 0; i < n - 1; i++) {
+	            if (numbers[i] * a + b != numbers[i + 1]) {
+	                isSolved = false;
 	                break;
 	            }
 	        }
-	    }
-	    
-	    return isSame;
+	        
+	        System.out.print(isSolved ? numbers[n - 1] * a + b : "B");
+		}
 	}
 }
